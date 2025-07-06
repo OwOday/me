4fcff1a825195ffd21e92dafa201cca586b5eb91
no <no>
2025-07-05 23:13:53 -0700

    forgot what I added

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+mkdir -p $HOME/tools
 cp -r ./bedrock/home/bee $HOME/..
 git clone https://github.com/OwOday/graudit $HOME/tools/graudit
 git clone https://github.com/OwOday/xbps-pkg $HOME/tools/xbps-pkg
